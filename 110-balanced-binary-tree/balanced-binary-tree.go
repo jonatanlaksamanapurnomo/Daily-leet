8bf194905dadf038b76ac88d6adeb33a8a3dab4d
Jonathan Laksamana Purnomo <39803159+jonatanlaksamanapurnomo@users.noreply.github.com>
2026-02-08 10:07:22 +0700

    Time: 0 ms (100.00%) | Memory: 7.4 MB (12.05%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.go
+++ 110-balanced-binary-tree/balanced-binary-tree.go
@@ -6,8 +6,10 @@
  *     Right *TreeNode
  * }
  */
+
+ const NOT_BALANCE_FLAG = -100
 func isBalanced(root *TreeNode) bool {
-    return height(root) != -100
+    return height(root) != NOT_BALANCE_FLAG
 }
 
 func height(root *TreeNode) int {
@@ -17,17 +19,17 @@ func height(root *TreeNode) int {
 
     left := height(root.Left)
     if left == -100 {
-        return -100
+        return NOT_BALANCE_FLAG
     }
 
     right := height(root.Right)
-    if right == -100{
-        return -100
+    if right == NOT_BALANCE_FLAG{
+        return NOT_BALANCE_FLAG
     }
 
     //Checking balance or not
     if abs(left - right) > 1 {
-        return -100
+        return NOT_BALANCE_FLAG
     }
 
 
