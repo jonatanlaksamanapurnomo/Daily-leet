078307b42780636b58a85d86b7d97f51cddacea9
Jonathan Laksamana Purnomo <39803159+jonatanlaksamanapurnomo@users.noreply.github.com>
2026-02-16 20:30:24 +0700

    Time: 44 ms (10.21%) | Memory: 90.8 MB (36.55%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,25 +1,31 @@
 class Solution {
-      public boolean containsNearbyDuplicate(int[] nums, int k) {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
         int left = 0;
         HashMap<Integer,Integer> cache = new HashMap<>();
 
-        for(int right = 0 ; right < nums.length ; right++){
+
+        for(int i = 0 ; i<Math.min(k,nums.length) ; i++){
+            if(cache.getOrDefault(nums[i] , 0 ) > 0){
+                return true;
+            }
+            cache.put(nums[i] , cache.getOrDefault(nums[i] , 0) + 1);
+        }
+
+        for(int right = k ; right < nums.length ; right++){
 
             //Shrink window if already > k
             if(right - left + 1 > k+1){
-                if(cache.containsKey(nums[left])){
-                    cache.put(nums[left] , cache.get(nums[left]) - 1);
-                    left++;
-                }
+                cache.put(nums[left] , cache.get(nums[left]) - 1);
+                left++;
             }
 
-           //Check elm already exist or not
+            //Check elm already exist or not
             int currentElm = nums[right];
-            if(cache.containsKey(currentElm) && cache.getOrDefault(currentElm,0) >= 1) {
+            if (cache.getOrDefault(currentElm, 0) > 0) {
                 return true;
             }
 
-            cache.put(nums[right] ,1);
+            cache.put(nums[right] ,cache.getOrDefault(nums[right],0) + 1);
         }
 
         return false;
