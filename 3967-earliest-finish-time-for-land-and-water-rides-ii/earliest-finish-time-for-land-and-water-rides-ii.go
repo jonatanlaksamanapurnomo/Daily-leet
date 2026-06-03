a2eb853999501ccd18d3f5737b5b5d7a8cc82e6e
Jonathan Laksamana Purnomo <39803159+jonatanlaksamanapurnomo@users.noreply.github.com>
2026-06-03 09:09:11 +0700

    Time: 0 ms (100.00%) | Memory: 14.1 MB (33.33%) - LeetSync

---
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.go
@@ -0,0 +1,40 @@
+func earliestFinishTime(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+    ans := math.MaxInt32
+
+    minLandFinish , minWaterFinish := math.MaxInt32 , math.MaxInt32
+
+    for idx := range landStartTime{
+        minLandFinish = min(minLandFinish , landStartTime[idx] + landDuration[idx])
+    }
+
+    for idx := range waterStartTime{
+        minWaterFinish = min(minWaterFinish , waterStartTime[idx] + waterDuration[idx])
+    }
+
+
+    //min finish + duration
+    for idx,waterLeadDuration := range waterDuration{
+        waitingTime := waterStartTime[idx] - minLandFinish
+        if waitingTime <= 0 {
+            waitingTime = 0
+        }
+        ans = min(ans , minLandFinish + waterLeadDuration + waitingTime )
+    }
+
+    for idx,landLeadDuration := range landDuration {
+        waitingTime := landStartTime[idx] - minWaterFinish
+        if waitingTime <= 0 {
+            waitingTime = 0
+        }
+        ans = min(ans ,minWaterFinish + landLeadDuration + waitingTime)
+    }
+    return ans
+}
+
+
+func min(a,b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
